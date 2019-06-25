11e5fbac04660478cca3cae66ba7b1b0ed997e63
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-06-25 20:34:44 +0300

    валидация

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,25 +1,33 @@
-$('.js-btn-submit').on('click', function (event) {
+$(function(){
+  $('#chat-form').on('submit', function (event) {
 
-  let postTable = $('#post-table');
-  let chatForm = $('#chat-form');
+    let postTable = $('#post-table');
+    let chatForm = $('#chat-form');
+    chatForm.find('.invalid-feedback').remove();
+    chatForm.removeClass('was-validated');
 
-  event.preventDefault();
+    event.preventDefault();
 
-  $.ajax({
-    type: chatForm.attr('method'),
-    url: chatForm.attr('action'),
-    data: chatForm.serialize(),
-  }).done(function(data) {
-    postTable.html(data);
-    chatForm[0].reset();
+    $.ajax({
+      type: chatForm.attr('method'),
+      url: chatForm.attr('action'),
+      data: chatForm.serialize(),
+    }).done(function(data) {
+      postTable.html(data);
+      chatForm[0].reset();
+      chatForm.find('.invalid-feedback').remove();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      let $field, fieldName, $feedback;
+
+      chatForm.addClass('was-validated');
+
+      for (fieldName in jqXHR.responseJSON) {
+        $field = $('[name*="[' + fieldName + ']"]');
+        $feedback = $('<div class="invalid-feedback"></div>');
+        $feedback.html(jqXHR.responseJSON[fieldName]);
+        $field.parent().append($feedback);
+      }
+    });
+
   });
-
 });
-
-
-
-
-
-
-
-
